d8491a2b9d03640fa70249e1fb860d7d558db236
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-19 20:14:33 +0100

    add base database read/write functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
+//Pending tasks:
+// - fix reset db on reload
+// - add favicon
+// - edit no ToDo entered alert
+
 import { useState, useEffect } from 'react'
 import uuid from 'react-uuid';
 import Task from './Components/Task'
 import './App.css'
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, update, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://todolist-396ab-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
+const userID = 1234
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const toDoDB = ref(database, "toDo")
+const toDoDB = ref(database, "toDoApp")
+const toDoListRef = ref(database, '/toDoApp/toDoLists/' + userID)
 
 function App() {
-  const [toDos, setToDos] = useState( () => 
-    JSON.parse(localStorage.getItem("toDos")) || []
-    )
+  const [toDos, setToDos] = useState([])
   const [newText, setNewText] = useState("")
+  
+  //Read tasks from database
+  useEffect(() => {
+    onValue(toDoListRef, (snapshot) => {
+      setToDos(snapshot.val() || []);
+    });
+  }, []);
 
-  const toDoElements = toDos.map(task => <Task 
-  key={task.id}
-  id={task.id}
-  text={task.text}
-  checked={task.checked}
-  handleChange={handleChecked}
-  deleteTask={deleteTask}
-  />)
+  const toDoElements = toDos.map(task => (
+    <Task
+      key={task.id}
+      id={task.id}
+      text={task.text}
+      checked={task.checked}
+      handleChange={handleChecked}
+      deleteTask={deleteTask}
+    />
+  ));
 
-  function createNewToDo() {
+  function createNewToDo(event) {
+    event.preventDefault()
     if(newText != ""){
       const newTask = { id: uuid(), text: newText, checked: false };
       setToDos(prevToDos => [...prevToDos, newTask])
@@ -57,24 +72,28 @@ function App() {
     })
   }
 
+  //function called by following useEffect to write new toDo to database
+  function writeNewToDo(toDoList) {
+    const updates = {};
+    updates['/toDoLists/' + userID] = toDoList;
+  
+    return update(toDoDB, updates);
+  }
+
   useEffect(() => {
-    localStorage.setItem("toDos", JSON.stringify(toDos))
-    push (toDoDB, JSON.stringify(toDos))
+    writeNewToDo(toDos)
   }, [toDos])
 
   function handleReset() {
-    localStorage.clear()
     window.location.reload(false);
   }
-
-
 
   return (
     <div className="app">
       <h1 className='title'>ToDo List</h1>
       <div className='newtask'>
         <form className='newTaskForm' onSubmit={createNewToDo}>
-          <input className='newtaskinput' type="text" name="newtask" value={newText} onChange={updatNewText} placeholder="Type new ToDo here"/>
+          <input className='newtaskinput' type="text" name="newtask" value={newText} onChange={updatNewText} placeholder="Type new ToDo here" autoComplete='off'/>
           <i className='newtaskbutton fa-solid fa-circle-plus' onClick={createNewToDo}></i>
         </form>
       </div>

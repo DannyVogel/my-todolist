d1d94c13591aa8598e02d0c93adab5ef656db8ca
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-08 10:06:29 +0200

    create unit tests for app component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -12,4 +12,31 @@ describe('<App />', () => {
   it('renders without errors', () => {
     render(<App />);
   });
+  it('renders title', () => {
+    render(<App />);
+    const element = screen.getByText("ToDo List");
+    expect(element).toBeInTheDocument();
+  });
+  it('renders default task', () => {
+    render(<App />) 
+    const element = screen.getByText("Your ToDos will appear here")
+    expect(element).toBeInTheDocument();
+  });
+  
+  // let toDos = [
+  //   {
+  //     id: 1,
+  //     text: 'test',
+  //     checked: false
+  //   },
+  //   {
+  //     id: 2,
+  //     text: 'test2',
+  //     checked: true
+  //   }
+  // ]
+  // it('renders App component', () => {
+  //   render(<App />);
+  //   expect(screen.getByTestId('taskEl')).toBeInTheDocument();
+  // });
 });

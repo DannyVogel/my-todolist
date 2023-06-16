f0e47917f8f252115bf6a0c0c218cdcf6b1a4781
DannyVogel <danny.hj.vogel@gmail.com>
2023-06-16 21:54:53 +0200

    fix: image loading

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -4,7 +4,7 @@ export default function Loader(props: { darkMode: boolean, isUnmounting: boolean
     <div className={`${props.darkMode && 'dark'} ${props.isUnmounting && ''} loader`}>
         <div className="loader-wrapper">
           <h1 className='title'>ToDo List</h1>
-          <img className='loader-image' src="public/notebook-small.png" alt="notebook icon enlarged image" width={250} />
+          <img className='loader-image' src="https://cdn-icons-png.flaticon.com/512/4341/4341063.png" alt="notebook icon enlarged image" width={250} />
           <h2 className='loader-text'>Loading...</h2>
         </div>
     </div>
